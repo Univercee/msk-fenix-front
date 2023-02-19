9c08467ee1ace3b6ed71afbfce01a44714c62a13
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2023-02-19 21:02:14 +0300

    connect with backend

--- src/components/Products/ProductCard.vue
+++ src/components/Products/ProductCard.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="mb-4">
     <div class="product-card d-flex flex-column">
-      <div class="product-card__image">
-        <img src="../../assets/images/product.jpg" alt="image">
+      <p class="text-center">{{image_index+1}}/{{product_data.images.length}}</p>
+      <div class="product-card__image d-grid">
+        <p class="product-card__image-controls" @click="prevImageIndex()">-</p>
+        <img :src="VUE_APP_DATA_SERVICE_URL+'/storage/images/'+product_data.images[image_index]" alt="image">
+        <p class="product-card__image-controls" style="justify-self:end" @click="nextImageIndex()">+</p>
       </div>
       <div class="p-2 product-card__body">
         <div class="mt-2">
-          <p class="product-card__repair">Евроремонт</p>
+          <p class="product-card__repair">{{product_data.repair_type}}</p>
         </div>
         <div class="mt-2">
-          <p class="product-card__price">3 000 000 ₽</p>
-          <p class="product-card__price-for-meter">100 000 ₽/м.кв.</p>
+          <p class="product-card__price">{{ product_data.price }} ₽</p>
+          <p class="product-card__price-for-meter">{{ parseInt(product_data.price/product_data.area) }} ₽/м.кв.</p>
         </div>
         <div class="d-flex mt-2">
-          <p class="product-card__rooms">2-комн. квартира</p>
-          <p class="product-card__area">10 м<sup>2</sup></p>
-          <p class="product-card__floor">5/10 эт.</p>
+          <p class="product-card__rooms" v-if="product_data.rooms">{{ product_data.rooms }}-комн. квартира</p>
+          <p class="product-card__rooms" v-else>Студия</p>
+          <p class="product-card__area">{{ product_data.area }} м<sup>2</sup></p>
+          <p class="product-card__floor">{{ product_data.floor }}/{{ product_data.floor_amount }} эт.</p>
         </div>
         <div class="mt-2">
-          <p class="product-card__address">г.Москва ул.Пушкина д.5</p>
-          <p class="product-card__metro">метро Пушкинская</p>
+          <p class="product-card__address">{{ product_data.address }}</p>
+          <p class="product-card__metro">метро {{ product_data.metro }}</p>
         </div>
         <div class="d-flex justify-content-center align-items-center mt-3 w-100">
           <a :href="'tel:'+socials.Phone.formatted_value" class="btn btn-outline-primary" style="margin-right:5px">{{ socials.Phone.formatted_value }}</a>
@@ -32,11 +36,22 @@
 
 <script>
 export default {
-    props:{
-        product_data:{
-            required: true,
-            type: Array
-        }
+    props:['product_data'],
+    data(){
+      return{
+        image_index: 0,
+        VUE_APP_DATA_SERVICE_URL: process.env.VUE_APP_DATA_SERVICE_URL
+      }
+    },
+    methods:{
+      nextImageIndex(){
+        this.image_index = (this.image_index+1)%this.product_data.images.length
+      },
+      prevImageIndex(){
+        this.image_index = this.image_index-1
+        this.image_index = this.image_index < 0 ? this.product_data.images.length + this.image_index : this.image_index
+        this.image_index = this.image_index%this.product_data.images.length
+      }
     }
 }
 </script>
@@ -55,11 +70,19 @@ export default {
     }
 
     &__image{
-      width: 100%;
-
+      
       & img{
-        object-fit: cover;
+        object-fit: contain;
         width: 100%;
+        height: 300px;
+      }
+
+      &-controls{
+        position:absolute;
+        align-self: center;
+        font-size: 30px;
+        background-color: white;
+        cursor: pointer;
       }
     }
 
@@ -92,14 +115,6 @@ export default {
       padding-left: 5px;
       border-left: solid lightgray 2px;
       font-weight: bold;
-    }
-
-    &__address{
-
-    }
-
-    &__metro{
-
     }
   }
 </style>
--- src/components/Products/Products.vue
+++ src/components/Products/Products.vue
@@ -2,19 +2,29 @@
     <div>
         <h2 class="mb-5 text-center" style="font-weight: bold;">Объекты недвижимости</h2>
         <div class="row d-flex justify-content-start">
-            <c-product-card class="col-lg-4"></c-product-card>
-            <c-product-card class="col-lg-4"></c-product-card>
-            <c-product-card class="col-lg-4"></c-product-card>
-            <c-product-card class="col-lg-4"></c-product-card>
-            
-            
+            <c-product-card v-for="estate in estates" :key="estate" :product_data="estate" class="col-lg-4"></c-product-card>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            estates: []
+        }
+    },
+    mounted(){
+        this.fetch()
+    },
+    methods:{
+        fetch(){
+            axios.get('estates').then(response=>{
+                this.estates = response.data.estates
+                console.log(response.data.estates);
+            })
+        }
+    }
 }
 </script>
 
